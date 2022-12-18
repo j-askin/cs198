ff90422e8426c456ff976cd705b6ddd62bc86fcb
j-askin <91054766+j-askin@users.noreply.github.com>
2022-12-18 20:20:09 +0800

    Updated script for training

--- folder_for_training/train.bat
+++ folder_for_training/train.bat
@@ -1,5 +1,10 @@
 :start
 
+pip3 install torch torchvision torchaudio
+pip install -U openmim
+mim install mmcv-full
+pip install mmsegmentation
+pip install opencv-python
 python tools/train.py mmsegmentation/configs/pspnet/pspnet_r50-d8_512x1024_40k_cityscapes.py --gpu-id 0
 
 pause
